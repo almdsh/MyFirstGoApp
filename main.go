4fdc6d40f90b6a017014a88cd3e7d0dce8ec0e1f
Alim Dyshekov <almdsh@mail.ru>
2025-02-19 15:40:11 +0300

    Ex. 2 - http server on port 127.0.0.1:9090

--- main.go
+++ main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
-type Handler struct{}
-
-func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	_, err := w.Write([]byte("Hello, world\n"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", req.URL.Path)
-}
-
 func main() {
-	log.Println(http.ListenAndServe("127.0.0.1:9090", &Handler{}))
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		_, err := w.Write([]byte("hello"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
 
+	log.Println("Сервер запущен на http://127.0.0.1:9090/hello")
+	log.Println(http.ListenAndServe(":9090", nil))
 }
