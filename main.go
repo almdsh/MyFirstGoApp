cc3460b9aade9e46f14daf767ab257eb43d727f8
Alim Dyshekov <almdsh@mail.ru>
2025-02-19 17:23:33 +0300

    С помощью ИИ - http и html

--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// HTML шаблон с формой
+	const tmpl = `
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <form method="POST">
+                <input type="text" name="message">
+                <input type="submit" value="Отправить">
+            </form>
+            {{if .Message}}
+                <h2>Ваше сообщение: {{.Message}}</h2>
+            {{end}}
+        </body>
+    </html>
+    `
+
+	t := template.Must(template.New("form").Parse(tmpl))
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		_, err := w.Write([]byte("hello"))
+		data := struct {
+			Message string
+		}{}
+
+		// Обработка POST запроса
+		if req.Method == http.MethodPost {
+			data.Message = req.FormValue("message")
+		}
+
+		// Отображаем шаблон
+		err := t.Execute(w, data)
 		if err != nil {
-			log.Println(err)
+			log.Printf("Template error: %v", err)
 		}
 	})
 
-	log.Println("Сервер запущен на http://127.0.0.1:9090/hello")
 	log.Println(http.ListenAndServe(":9090", nil))
 }
