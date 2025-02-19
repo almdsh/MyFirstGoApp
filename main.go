0ad8aae308de9c2f71dc1696784bd9590b4eb189
Alim Dyshekov <almdsh@mail.ru>
2025-02-19 17:49:20 +0300

    Ересь какая-то

--- main.go
+++ main.go
@@ -1,46 +1,37 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	// HTML шаблон с формой
-	const tmpl = `
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <form method="POST">
-                <input type="text" name="message">
-                <input type="submit" value="Отправить">
-            </form>
-            {{if .Message}}
-                <h2>Ваше сообщение: {{.Message}}</h2>
-            {{end}}
-        </body>
-    </html>
-    `
+	r := mux.NewRouter()
+	r.HandleFunc("/products/{key}", ProductHandler)
+	r.HandleFunc("/articles/{category}/", ArticlesCategoryHandler)
+	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+	http.Handle("/", r)
 
-	t := template.Must(template.New("form").Parse(tmpl))
-
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		data := struct {
-			Message string
-		}{}
-
-		// Обработка POST запроса
-		if req.Method == http.MethodPost {
-			data.Message = req.FormValue("message")
-		}
-
-		// Отображаем шаблон
-		err := t.Execute(w, data)
-		if err != nil {
-			log.Printf("Template error: %v", err)
-		}
-	})
-
+	// Add these lines
+	fmt.Println("Server is running on http://localhost:8080")
 	log.Println(http.ListenAndServe(":9090", nil))
+}
+
+func ProductHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Key: %v\n", vars["key"])
+}
+func ArticlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
+func ArticleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\\%v\n", vars["category"], vars["id"])
 }
