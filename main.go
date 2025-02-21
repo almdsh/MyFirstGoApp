28a2b2dfa3defeef60a97eb72e1430a6294cd80c
Alim Dyshekov <almdsh@mail.ru>
2025-02-21 11:59:35 +0300

    Сервер Флориана

--- main.go
+++ main.go
@@ -1,25 +1,99 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 func main() {
-	client := http.Client{}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleRoot)
 
-	resp, err := client.Get("https://golangforall.com/en/")
+	mux.HandleFunc("POST /users", createUser)
+	mux.HandleFunc("GET /users/{id}", getUser)
+	mux.HandleFunc("DELETE /users/{id}", deleteUser)
+
+	fmt.Printf("Server is running on port 8080")
+	http.ListenAndServe(":8080", mux)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+type User struct {
+	Name string `json:"name"`
+}
+
+var userCache = make(map[int]User)
+
+var cacheMutex sync.RWMutex
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
-	buf, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
+	if user.Name == "" {
+		http.Error(w, "Name is required", http.StatusBadRequest)
+		return
 	}
 
-	fmt.Println(string(buf))
+	cacheMutex.Lock()
+	userCache[len(userCache)+1] = user
+	cacheMutex.Unlock()
 
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	cacheMutex.RLock()
+	user, ok := userCache[id]
+	cacheMutex.RUnlock()
+	if !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if _, ok := userCache[id]; !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	cacheMutex.Lock()
+	delete(userCache, id)
+	cacheMutex.Unlock()
+
+	w.WriteHeader(http.StatusNoContent)
 }
